083f36af0e10d6dbc51310f3f3373370196639f5
Sithiroth Ek <sithroth2@gmail.com>
2026-02-02 11:24:32 +0700

    update new files

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 // console.log("Hello World")
 
 
-// // syntax
+// // syntax ////
 // while (condition) {
 //     // code to be executed
 // }
@@ -30,17 +30,112 @@
 // console.log(addOne)
 
 // Arrays, Objects
-let num = [1,2,3,4,5]
-let [a, b] = num
-console.log(a)
-console.log(b)
+// let num = [1,2,3,4,5]
+// let [a, b] = num
+// console.log(a)
+// console.log(b)
 
-let car = {
-    'name': 'corolla',
-    'brand': 'toyota',
-    'color': 'white'
+// let car = {
+//     'name': 'corolla',
+//     'brand': 'toyota',
+//     'color': 'white'
+// }
+
+// let {name, brand, color} = car
+// console.log(name)
+// console.log(brand)
+
+// let num1 = [1,2,3,4]
+// let result = num1.map((num)=> {num * 2})
+// let even =num1.filter((num) => num %2 ==0)
+// console.log(even)
+// console.log(result)
+
+////Function ////
+
+// function one(a, b){
+//     return a/b
+// }
+// console.log(one(5, 10))
+
+// //..............
+// const hello = function(name){
+//     console.log(`Hello ${name}`)
+// }
+
+// hello("Roth")
+//..............
+
+// const hello = (name) => console.log(`Hello ${name}`)    
+
+//..................
+// function greet(){
+//     console.log("hello World")
+// }
+
+// const  greet = () =>{
+//     console.log("Hello World")
+//     console.log("Hello World")
+// }
+
+// function sayName(name) {
+//     console.log(`Hello, ${name}`)
+// }
+
+// greet()
+// sayName("Roth")
+
+//// Higher order functions////
+
+// const alertHello = () => {
+//     setTimeout(() => alert("Hello!"), 3000)
+// }
+// alertHello()
+
+//....................
+
+// const logHello = () =>{
+//     let count = 0
+//     const intervalID = setInterval (() => {
+//         console.log("Hello")
+//         count++
+//         if (count === 5) {
+//             clearInterval( intervalID)
+//         }
+//     }, 1000)
+// }
+// logHello()
+
+//// map() ////
+// const n =[1, 2, 3, 4, 5];
+// const square = n.map((num) => num * num);
+// console.log(square);
+
+// filter() ////
+// const n = [1, 2, 3 ,4, 5];
+// const even = n.filter((num) => num% 2 === 0);
+// console.log(even); 
+
+////
+
+function calculateAverage(){
+    
 }
 
-let {name, brand, color} = car
-console.log(name)
-console.log(brand)
+
+// Callback
+// sum(diplayConsole, 1, 2);
+// function sum(callback, x, y){
+//     let result = x + y;
+//     callback(result);
+// }
+
+// function displayConsole(result){
+//     console.log(result);
+// }
+
+// function displayPage(result){
+//     document.getElementById("myH1").textContent = result;
+// }
+
+
